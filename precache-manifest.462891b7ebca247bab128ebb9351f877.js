3c9324fc4bd74d576fb9ccb78fc80c07d6d04aaa
Rob Upchurch <robertc.upchurch@gmail.com>
2019-03-14 09:09:23 -0600

    Updates

---
+++ precache-manifest.462891b7ebca247bab128ebb9351f877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed406e152ff7949a235d",
+    "url": "/starGame/static/css/main.990a6211.chunk.css"
+  },
+  {
+    "revision": "ed406e152ff7949a235d",
+    "url": "/starGame/static/js/main.ed406e15.chunk.js"
+  },
+  {
+    "revision": "df7d51fd2ddfc392e57e",
+    "url": "/starGame/static/js/1.df7d51fd.chunk.js"
+  },
+  {
+    "revision": "f484e49d280144010267",
+    "url": "/starGame/static/js/runtime~main.f484e49d.js"
+  },
+  {
+    "revision": "2b0b2c63ade67c10421f7706d50e2e84",
+    "url": "/starGame/static/media/earth1.2b0b2c63.png"
+  },
+  {
+    "revision": "a5444f893a88c3fe54aa9cfb5dfaafe5",
+    "url": "/starGame/index.html"
+  }
+];
